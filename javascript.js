71a94a9698562373bf1c807206adfd78d7d3aede
Lim Joe Quan <joequanlim@gmail.com>
2025-12-28 23:07:06 +0800

    Add book colour choices and fixed book repetition
    Book colour is now selected with preset radio button options instead of
    the colour picker
    Fixed library repeating existing books when a new book was added

--- javascript.js
+++ javascript.js
@@ -1,31 +1,58 @@
 let library = []
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, colour) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    this.colour = colour
+    this.isWhiteText = false
     this.id = crypto.randomUUID()
 }
 
-function addBook(title, author, pages, read,) {
-    let book = new Book (title, author, pages, read,)
+function addBook(title, author, pages, read, colour) {
+    let book = new Book (title, author, pages, read, colour)
     library.push(book) 
     displayBook()
 }
 
+function determineBookColour(colour) {
+    for (book of library) {
+        if (colour === "#0059ff" || colour === "#e30000" || colour === "#ae00ff" || colour === "#875801" || colour === "#ff00d9") {
+            book.isWhiteText = true
+        } else {
+            book.isWhiteText = false
+        }
+    }
+}
+
+function clearForm(title, author, pages, read, colour) {
+    title.value = ""
+    author.value = ""
+    pages.value = ""
+    read.checked = false
+    colour.checked = false
+}
+
 function displayBook() {
     let libraryGrid = document.querySelector(".library-grid")
+
+    //This removes all existing books
+    libraryGrid.replaceChildren()
+
     for (book of library) {
         let bookCard = document.createElement("div")
         bookCard.classList.add("card")
+        bookCard.style.backgroundColor = book.colour
 
         let bookTitle = document.createElement("h3")
         bookTitle.textContent = book.title
+        bookTitle.classList.add("title")
         bookCard.appendChild(bookTitle)
 
         let bookAuthor = document.createElement("p")
         bookAuthor.textContent = "By: " + book.author
+        bookAuthor.classList.add("author")
         bookCard.appendChild(bookAuthor)
 
         let bookPages = document.createElement("p")
@@ -46,6 +73,10 @@ function displayBook() {
         }
         bookRead.appendChild(bookReadCheckbox)
         bookCard.appendChild(bookRead)
+        determineBookColour(book.colour)
+        if (book.isWhiteText === true) {
+            bookCard.classList.add("white-text")
+        }
         libraryGrid.appendChild(bookCard)
     }
 }
@@ -56,7 +87,9 @@ addBookBtn.addEventListener('click', () =>{
     let author = document.querySelector("#author")
     let pages = document.querySelector("#pages")
     let read = document.querySelector("#read")
-    if (title.value !== "" && author.value !== "" && pages.value !== "" && read.value !== "") {
-        addBook(title.value, author.value, pages.value, read.checked)
+    let colour = document.querySelector("input[name=colour]:checked")
+    if (title.value !== "" && author.value !== "" && pages.value !== "" && read.value !== "" && colour.value !== "") {
+        addBook(title.value, author.value, pages.value, read.checked, colour.value)
     }
+    clearForm(title, author, pages, read, colour)
 })
